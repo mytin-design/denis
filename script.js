f57f63c715fb9881fd77957cb93910d558adf6e6
mytin-design <denismytin@gmail.com>
2022-12-26 09:09:31 -0800

    -fourthcommit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.getElementById("dOpen").click();
 
 
 function showMore() {
-    var lnk = document.querySelector("a.u-lm-ln");
+    var lnk = document.querySelector("button.u-lm-ln");
     var sm = document.querySelector("p.u-d");
     sm.innerHTML = "3 years as a web developer. Expert in HTML, CSS, Bootstrap and Javascript. Over 9 years as an academic writer. Written at least 400 Disseratations and Thesis, and over 600 Essays, reflection papers, case studies, capstone projects and exams. Worked as an assistant medical research associate at Kenya Medical Research Institute and assistant Laboratory Technologist in Kenyatta University Health Unit. <br><br> Please reach out for more information."
     lnk.style.display = "none";
